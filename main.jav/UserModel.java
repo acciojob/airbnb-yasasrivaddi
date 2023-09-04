b61fd47761fbb5667c25c55f145ed9c00a3ecc44
yasasrivaddi <136231520+yasasrivaddi@users.noreply.github.com>
2023-09-04 20:57:01 +0530

    Create UserModel.java

---
+++ main.jav/UserModel.java
@@ -0,0 +1,17 @@
+public class UserModel {
+    private String userId;
+    private String userName;
+
+    public UserModel(String userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
